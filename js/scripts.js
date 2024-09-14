90ca88b82547acb6e3c4be87bb7dede5d820d973
Stevekim <131708216+Solsauti@users.noreply.github.com>
2024-09-14 15:41:15 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,15 +14,13 @@ document.querySelector('form').addEventListener('submit', function(e) {
         const videoContainer = document.getElementById('videoContainer');
         const videoSource = document.getElementById('videoSource');
         const medicalVideos = [
-            'https://www.example.com/video1.mp4',
-            'https://www.example.com/video2.mp4',
-            'https://www.example.com/video3.mp4'
+            'hospital.mp4'
         ];
 
         videoButton.addEventListener('click', () => {
-            // Choose a random video URL
+            
             const randomVideo = medicalVideos[Math.floor(Math.random() * medicalVideos.length)];
-            videoSource.src = randomVideo;
+            videoSource.src = hospital.mp4 ;
             videoContainer.style.display = 'block';
             document.getElementById('medicalVideo').play();
         });
